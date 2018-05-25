c3800c64ba8d045669e82e2d97d2e45ea90f15c6
snailking <39387494+snailking@users.noreply.github.com>
2018-05-26 00:27:21 +0200

    Add files via upload
    million and above display fix

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var lastSecondsUntilFull=100
 lastHatchTime=0
 var eggstohatch1=864
 var lastUpdate=new Date().getTime()
+var modalID=0
 function main(){
     console.log('test')
     modal = document.getElementById('myModal');
@@ -23,6 +24,34 @@ function controlLoopFaster(){
     setTimeout(controlLoopFaster,30)
 }
 function refreshData(){
+    
+    for(var i=0;i<10;i++){
+        getAdText(i,function(index,text){
+            console.log('textwas ',index,text)
+            var adTextdoc=document.getElementById('adHyperlink'+index)
+            text=text.substring(0,128)
+            adTextdoc.textContent=onlyLetters(text)
+        });
+    }
+    for(var i=0;i<10;i++){
+        getAdUrl(i,function(index,text){
+            text=onlyurl(text)
+            console.log('urlwas ',index,text+"^")
+            var adTextdoc=document.getElementById('adHyperlink'+index)
+            //console.log('validurl ',validurlsimple(text))
+            //if(onlyurl(text)){
+                adTextdoc.href="http://"+text
+            //}
+        });
+    }
+    for(var i=0;i<10;i++){
+        getAdPrice(i,function(index,text){
+            //console.log('adprice ',index,text)
+            var adTextdoc=document.getElementById('adEthPrice'+index)
+            adTextdoc.textContent=weiToDisplay(text)
+        });
+    }
+    //adEthPrice6
     var sellsforexampledoc=document.getElementById('sellsforexample')
     marketEggs(function(eggs){
         eggs=eggs/10
@@ -43,7 +72,7 @@ function refreshData(){
         if(lastNumEggs!=eggs){
             lastNumEggs=eggs
             lastUpdate=new Date().getTime()
-            updateEggNumber(formatEggs(eggs))
+            updateEggNumber(eggs/eggstohatch1)//formatEggs(eggs))
 
         }
         var timeuntilfulldoc=document.getElementById('timeuntilfull')
@@ -67,7 +96,7 @@ function refreshData(){
         var allnumshrimp=document.getElementsByClassName('numshrimp')
         for(var i=0;i<allnumshrimp.length;i++){
             if(allnumshrimp[i]){
-                allnumshrimp[i].textContent=translateQuantity(shrimp,0)
+                allnumshrimp[i].textContent=translateQuantity(shrimp)
             }
         }
         var productiondoc=document.getElementById('production')
@@ -75,13 +104,40 @@ function refreshData(){
     });
     updateBuyPrice()
     updateSellPrice()
-	updateSnailmasterPrice()
-	updateCurrentSnailmaster()
-    var prldoc = document.getElementById('playerreflink'); 
-	prldoc.textContent = window.location.protocol + '//' + window.location.host + window.location.pathname + "?ref=" + web3.eth.accounts[0]; 
-	var copyText = document.getElementById("copytextthing"); 
-	copyText.value = prldoc.textContent;
-
+    var prldoc=document.getElementById('playerreflink')
+    prldoc.textContent=window.location.origin+"?ref="+web3.eth.accounts[0]
+    var copyText = document.getElementById("copytextthing");
+    copyText.value=prldoc.textContent
+}
+function purchaseAd(){
+    getAdPrice(modalID,function(index,price){
+        adtextdoc=document.getElementById('adInputText')
+        adurldoc=document.getElementById('adInputUrl')
+        url=adurldoc.value
+        url=url.replace("http://","").replace("https://","").replace("http://www.","").replace("https://www.","")
+        console.log('purchaseadvalues ',price,adtextdoc.value,adurldoc.value,validurlsimple(url))
+        if(onlyLetters(adtextdoc.value)!=adtextdoc.value){
+            alert('Numbers, letters and punctuation only in ad text')
+            return
+        }
+        if(onlyurl(url)!=url){
+            alert('invalid url')
+            return
+        }
+        buyAd(index,adtextdoc.value,url,price,function(){
+            displayTransactionMessage()
+            removeModal2()
+        });
+    });
+}
+function setmid(number){
+    //console.log('set modal id ',number)
+    modalID=number
+    getAdPrice(modalID,function(index,text){
+        //console.log('adprice ',index,text)
+        var adTextdoc=document.getElementById('adEthPriceConfirm')
+        adTextdoc.textContent=weiToDisplay(text)
+    });
 }
 function updateEggNumber(eggs){
     var hatchshrimpquantitydoc=document.getElementById('hatchshrimpquantity')
@@ -94,11 +150,10 @@ function updateEggNumber(eggs){
     }
 }
 function hatchEggs1(){
-	ref = getQueryVariable('ref'); 
-	var blacklistedAddresses = [ "0x86060b7959451f44ea1a15bd2b2da22f28e6f3ce" ]; 
-	if (!ref || ref == web3.eth.accounts[0] || blacklistedAddresses.indexOf(ref) > -1) { 
-		ref=0; 
-	}
+    ref=getQueryVariable('ref')
+    if(!ref || ref==web3.eth.accounts[0]){
+        ref=0
+    }
     console.log('hatcheggs ref ',ref)
     hatchEggs(ref,displayTransactionMessage())
 }
@@ -110,7 +165,7 @@ function liveUpdateEggs(){
         }
         difference=(currentTime-lastUpdate)/1000
         additionalEggs=Math.floor(difference*lastNumShrimp)
-        updateEggNumber(formatEggs(lastNumEggs+additionalEggs))
+        updateEggNumber((lastNumEggs+additionalEggs)/eggstohatch1)//formatEggs(lastNumEggs+additionalEggs))
     }
 }
 function updateSellPrice(){
@@ -137,31 +192,6 @@ function updateBuyPrice(){
         });
     });
 }
-
-function updateSnailmasterPrice(){
-    var snailmasterpricedoc=document.getElementById('snailmasterprice')
-	getSnailmasterReq(function(req) {
-		snailmasterpricedoc.textContent = translateQuantity(req, 0);
-	});
-}
-
-
-function updateCurrentSnailmaster(){
-    var currentsnailmasterdoc=document.getElementById('currentsnailmaster')
-    ceoAddress(function(address) {
-		//currentsnailmaster.textContent=address;
-	});
-}
-
-
-function getFreeShrimp2(){
-    var ethtospenddoc=0.001//document.getElementById('freesnailspend')
-    weitospend=web3.toWei(ethtospenddoc,'ether')
-    getFreeShrimp(weitospend,function(){
-        displayTransactionMessage();
-    });
-}
-	
 function buyEggs2(){
     var ethtospenddoc=document.getElementById('ethtospend')
     weitospend=web3.toWei(ethtospenddoc.value,'ether')
@@ -176,21 +206,7 @@ function translateQuantity(quantity,precision){
     quantity=Number(quantity)
     finalquantity=quantity
     modifier=''
-    if(precision == undefined){
-        precision=0
-        if(quantity<10000){
-            precision=1
-        }
-        if(quantity<1000){
-            precision=2
-        }
-        if(quantity<100){
-            precision=3
-        }
-        if(quantity<10){
-            precision=4
-        }
-    }
+
     //console.log('??quantity ',typeof quantity)
     if(quantity>1000000){
         modifier='M'
@@ -204,18 +220,35 @@ function translateQuantity(quantity,precision){
         modifier='T'
         finalquantity=quantity/1000000000000
     }
+    if(precision == undefined){
+        precision=0
+        if(finalquantity<10000){
+            precision=1
+        }
+        if(finalquantity<1000){
+            precision=2
+        }
+        if(finalquantity<100){
+            precision=3
+        }
+        if(finalquantity<10){
+            precision=4
+        }
+    }
     if(precision==0){
         finalquantity=Math.floor(finalquantity)
     }
     return finalquantity.toFixed(precision)+modifier;
 }
-
+function removeModal2(){
+    $('#adModal').modal('toggle');
+}
 function removeModal(){
         modalContent.innerHTML=""
         modal.style.display = "none";
 }
 function displayTransactionMessage(){
-    displayModalMessage("Transaction Submitted. This can take a moment depending on the state of the Ethereum Network.")
+    displayModalMessage("Transaction Submitted")
 }
 function displayModalMessage(message){
     modal.style.display = "block";
@@ -273,6 +306,12 @@ function disableButtons(){
             allnumshrimp[i].style.display="none"
         }
     }
+    var allnumshrimp=document.getElementsByClassName('btn-md')
+    for(var i=0;i<allnumshrimp.length;i++){
+        if(allnumshrimp[i]){
+            allnumshrimp[i].style.display="none"
+        }
+    }
 }
 function enableButtons(){
     var allnumshrimp=document.getElementsByClassName('btn-lg')
@@ -281,8 +320,56 @@ function enableButtons(){
             allnumshrimp[i].style.display="inline-block"
         }
     }
+        var allnumshrimp=document.getElementsByClassName('btn-md')
+    for(var i=0;i<allnumshrimp.length;i++){
+        if(allnumshrimp[i]){
+            allnumshrimp[i].style.display="inline-block"
+        }
+    }
 }
+function onlyLetters(text){
+    return text.replace(/[^0-9a-zA-Z\s\.!?,]/gi, '')
+}
+function checkOnlyLetters(str){
+    var pattern=new RegExp('^[0-9a-zA-Z\s\.!?,]*$')
+      if(!pattern.test(str)) {
+        return false;
+      } else {
+        return true;
+      }
+}
+function onlyurl(str){
+     return str.replace(/[^0-9a-zA-Z\.?&\/\+#=\-_:]/gi, '')
+}
+function validurlsimple(str){
+    var pattern=new RegExp('^[a-z0-9\.?&\/\+#=\-_:]*$')
+      if(!pattern.test(str)) {
+        return false;
+      } else {
+        return true;
+      }
+}
+function ValidURL(str) {
+  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+  if(!pattern.test(str)) {
+    alert("Please enter a valid URL.");
+    return false;
+  } else {
+    return true;
+  }
+}
+function callbackClosure(i, callback) { 
+    return function() {
+        return callback(i); 
+    } 
+} 
 web3.version.getNetwork((err, netId) => {
+    
     if(netId!="1"){
         displayModalMessage("Please switch to Ethereum Mainnet "+netId)
         disableButtons()
